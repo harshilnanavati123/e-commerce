9891cb8a1f55953763d06a3c7f1e0054b76eca0c
harshilnanavati123 <92220964+harshilnanavati123@users.noreply.github.com>
2023-04-24 21:29:25 +0530

    routes

--- admin.js
+++ admin.js
@@ -1,153 +1,21 @@
-const Product = require('../models/product')
-// const mongodb = require('mongodb')
-const {validationResult} = require('express-validator')
-exports.addController = (req, res, next) => {
-    res.render('admin/edit-product', {
-        Title: 'Productt',
-        editing: false,
-        hasErrors:false,
-        errorMessage:null
-    })
-
-}
-
-exports.setController = (req, res, next) => {
-    // const NewObject = mongodb.ObjectId
-    // const Id = new NewObject(req.user._id)
-    // console.log(user)
-    const title = req.body.title;
-    const image = req.file;
-    const price = req.body.price;
-    const description = req.body.description;
-    if(!image){
-        return res.status(422).render('admin/edit-product', {
-            product: product, Title: 'Add Products',
-            path: '/edit-product',
-            editing: false,
-            hasErrors:true,
-            errorMessage:'Attached file is not an image',
-            product:{title:title,
-            price:price,
-            description:description
-        }
-        })
-    }
-    const errors = validationResult(req)
-    if(!errors.isEmpty){
-       return res.status(422).render('admin/edit-product', {
-            product: product, Title: 'Add Products',
-            path: '/edit-product',
-            editing: false,
-            hasErrors:true,
-            errorMessage:errors.array()[0].msg,
-            product:{title:title,
-            imageUrl:imageUrl,
-            price:price,
-            description:description
-        }
-        })
-    }
-    const imageUrl = image.path
-    const product = new Product({
-        title:title,
-        price:price,
-        description:description,
-        imageUrl:imageUrl,
-        userId:req.user
-    });
-    product.save()
-        .then((result) => {
-            res.redirect('/')
-            console.log(result)
-        })
-        .catch(err => console.log(err))
-}
-exports.getEditProducts = (req, res, next) => {
-    const editMode = req.query.edit
-    console.log(editMode)
-    if (!editMode) {
-        return res.redirect('/')
-    }
-    const prodId = req.params.productId
-    // Product.findByPk(prodId)
-    Product.findById(prodId)
-        .then(product => {
-            if (!product) {
-                return res.redirect('/')
-            }
-            res.render('admin/edit-product', {
-                product: product, Title: 'Edit Products',
-                path: '/edit-product',
-                editing: editMode,
-                hasErrors:false,
-                errorMessage:null
-            })
-        }).catch(err => console.log(err))
-}
-
-exports.postEditProduct = (req, res, next) => {
-    const prodId = req.body.productId
-    const updatedTitle = req.body.title
-    const updatedPrice = req.body.price
-    const image = req.file
-    const updatedDescription = req.body.description
-    const errors = validationResult(req)
-    if(!errors.isEmpty){
-       return res.status(422).render('admin/edit-product', {
-          Title: 'Edit Products',
-            path: '/edit-product',
-            editing: true,
-            hasErrors:true,
-            errorMessage:errors.array()[0].msg,
-            product:{
-            title:updatedTitle,
-            price:updatedPrice,
-            description:updatedDescription,
-            _id:prodId
-        }
-        })
-    }
-Product.findById(prodId).then(product=>{
-    if(product.userId.toString()!==req.user._id.toString()){
-        return res.redirect('/')
-    }
-    product.title=updatedTitle;
-    product.price=updatedPrice;
-    if(image){
-    product.imageUrl=image.path
-    }
-    product.description=updatedDescription
-    return product.save().then(result => {
-        res.redirect('/products')
-    })
-})
-
-}
-
-exports.postDeleteeleteProduct = (req, res, next) => {
-
-    const prodId = req.body.productId
-    Product.deleteOne({_id:prodId,userId:req.user._id})
-        .then(() => {
-            res.redirect('/products')
-        })
-        .catch(err => console.log(err))
-
-
-}
-
-exports.getProducts = (req, res, next) => {
-    Product.find({userId:req.user._id})
-    // .select('title price -_id')
-    // .populate('userId','name')
-        .then(product => {
-            console.log(product)
-            res.render('admin/products', {
-                prods: product, Title: 'Admin Products',
-                path: '/products',
-                isAuthenticated: req.session.isLoggedIn
-            })
-        }
-        ).catch(err => console.log(err))
-
-}
+const express = require('express')
+const {body} = require('express-validator')
+const path = require('path')
+const route = express.Router()
+const isAuth = require('../middleware/is-auth')
+const adminController = require('../controllers/admin')
+route.get('/add-product',isAuth,adminController.addController)
+route.get('/products',isAuth,adminController.getProducts)
+route.post('/add-product',[
+    body('title').isAlphanumeric().isLength({min:4}).trim(),
+    body('price').isFloat(),
+    body('description').isLength({min:5,max:200})
+],isAuth,adminController.setController)
+route.get('/edit-product/:productId',isAuth, adminController.getEditProducts)
+route.post('/edit-product',[
+    body('title').isString().isLength({min:4}).trim(),
+    body('price').isFloat(),
+    body('description').isLength({min:5,max:200})
+],isAuth,adminController.postEditProduct)
+route.post('/delete-product',isAuth,adminController.postDeleteeleteProduct)
+module.exports = route
--- auth.js
+++ auth.js
@@ -1,153 +1,52 @@
+const express = require('express')
+const route = express.Router()
+const {check,body} = require('express-validator')
+const authController = require('../controllers/auth')
 const User = require('../models/user')
-const bcrypt = require('bcryptjs')
-const crypto = require('crypto')
-const {validationResult}=require('express-validator')
-// const nodeMailer = require('nodemailer')
-// const sendGridTransport = require('nodemailer-sendgrid-transport')
-// const transporter = nodeMailer.createTransport(sendGridTransport({
-//     auth:{
-//         api_user:
+route.get('/login', authController.getLogin)
+route.get('/signup', authController.getSignUp)
+route.post('/login',[body('email')
+.isEmail()
+.withMessage('enter valid email')
+.normalizeEmail(),
+body('password')
+.isLength({min:'5'})
+.isAlphanumeric()
+.trim()
+.withMessage('enter valid password')
+ ],authController.postLogin)
+route.post('/signup',[
+    check('email')
+.isEmail()
+.withMessage('Please enter a valid email')
+.custom((value,{req})=>{
+//     if(value === 'test@test.com'){
+//         throw new Error('This email address is forbidden')
 //     }
-// }))
-
-exports.getLogin = (req, res, next) => {
- let message = req.flash('err')
-if(message.length>0){
-    message = message[0]
-}else{
-    message = null
-}
-    
-    res.render('auth/login', {
-        path: '/login',
-        Title: 'LoginPage',
-        errorMessage:message,
-        oldInput:{email:'',password:''}
-    })
-}
-
-exports.getSignUp = (req,res,next)=>{
-    let message = req.flash('err')
-if(message.length>0){
-    message = message[0]
-}else{
-    message = null
-}
-    res.render('auth/signup', {
-        path: '/signup',
-        Title: 'SignUp Page',
-        errorMessage:message,
-        oldInput:{email:'',password:'',condirmPassword:''}
-    })
-}
-
-exports.postLogin = (req, res, next) => {
-    const email = req.body.email
-    const password = req.body.password
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-       return res.status(422).render('auth/login', {
-            path: '/login',
-            Title: 'LoginPage',
-            errorMessage:errors.array()[0].msg,
-            oldInput:{email:email,password:password}
-        })
+//     return true
+return User.findOne({email:value})
+.then((userDoc)=>{   // finding user in database
+    if(userDoc){
+       return Promise.reject('Email already exist please try a new one')
     }
-    User.findOne({email:email}).then(user=>{
-        if(!user){
-            return res.status(422).render('auth/login', {
-                path: '/login',
-                Title: 'LoginPage',
-                errorMessage:'Invalid email or password',
-                oldInput:{email:email,password:password}
-            })
-        }
-        bcrypt.compare(password,user.password).then(doMatch=>{
-            if(doMatch){
-                req.session.user = user;
-                req.session.isLoggedIn = true
-                return req.session.save(err => {
-                    console.log(err)
-                    return res.redirect('/')
-                })
-            }
-            return res.status(422).render('auth/login', {
-                path: '/login',
-                Title: 'LoginPage',
-                errorMessage:'Invalid email or password',
-                oldInput:{email:email,password:password}
-            })
-        })
-    })
-   
-}
-exports.postLogout = (req, res, next) => {
-    req.session.destroy(err => {
-        console.log(err)
-        res.redirect('/')
-    })
-}
-
-exports.postSignUp = (req,res,next)=>{
-const email = req.body.email;
-const password = req.body.password;
-const errors = validationResult(req)
-if(!errors.isEmpty){
-    console.log(errors.array())
-    return res.status(422).render('auth/signup', {
-        path: '/signup',
-        Title: 'SignUp Page',
-        errorMessage:errors.array()[0],
-        oldInput:{email:email,password:password,confirmPassword:req.body.confirmPassword}
-    })
-}
-
- bcrypt.hash(password, 12)         //hashing the password
-   .then((hashedPassword)=>{                
-    const user = new User({                  //creating a new user
-        email:email,                         
-        password:hashedPassword,                //using hased password
-        cart:{items:[]}
-    })
-    return user.save() 
 })
-                    
-    .then(result=>{
-        res.redirect('/login')
-    }).catch(err=>console.log(err))
-}
-
-exports.getReset = (req,res,next)=>{
-    let message = req.flash('err')
-    if(message.length>0){
-        message = message[0]
-    }else{
-        message = null
-    }
-    res.render('auth/reset', {
-        path: '/reset',
-        Title: 'Reset Page',
-        errorMessage:message
-    })
-}
-
-exports.postReset = (req,res,next)=>{
-crypto.randomBytes(32,(err,buffer)=>{
-    if(err){
-        return res.redirect('/reset')
-    }
-    const token =  buffer.toString('hex')
-    User.findOne({email:req.body.email}).then(user=>{
-        if(!user){
-            req.flash('err','No account Found')
-           return res.redirect('/reset')
-        }
-        user.resetToken = token;
-        user.resetTokenExpiration = Date.now() + 3600000
-        return user.save()
-        .then(result=>{
-             res.redirect('/')
-        })
-    }).catch()
 })
-}
+.normalizeEmail()
+,body('password','Please enter valid password with min 5 char')
+.isLength({min:5})
+.isAlphanumeric()
+,
+body('confirmPassword').custom((value,{req})=>{
+   if(value!==req.body.password){
+    throw new Error('Passwords does not match')
+   }
+   return true
+})
+
+] ,authController.postSignUp)
+,route.post('/logout', authController.postLogout)
+route.get('/reset', authController.getReset)
+route.post('/reset',authController.postReset)
+module.exports = route
+
+
